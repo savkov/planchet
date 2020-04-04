6e4e8a1f98fdace8e7dcb886da892b242da76fba
Sasho Savkov <me@sasho.io>
2020-04-04 23:19:15 +0100

    Better check script

--- check_release.py
+++ check_release.py
@@ -2,10 +2,11 @@ import re
 import requests
 from pypi_cli import get_package
 import sys
+import os
 
 pack = get_package('planchet', requests.Session())
 versions = set(pack.version_downloads.keys())
-text = open('setup.py').read()
+text = open(os.path.join(os.path.dirname(__file__), 'setup.py')).read()
 version = re.search('version *= *\'([0-9.vab]+)\'', text).group(1)
 
 if version in versions:
