4529b8e897db21412b84ce4c35449101847bd923
Sasho Savkov <me@sasho.io>
2020-05-16 13:17:31 +0100

    🛠️ moved cc reporter download to docker

--- test-coverage.sh
+++ test-coverage.sh
@@ -2,10 +2,8 @@ if [ -z "$CC_TEST_REPORTER_ID" ]
 then
   pytest .
 else
-  curl -L -o cc-test-reporter https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64
-  chmod +x cc-test-reporter
-  cc-test-reporter before-build
+  ./cc-test-reporter before-build
   pytest -v --cov-config .coveragerc --cov .
   coverage xml
-  cc-test-reporter after-build -t coverage.py --exit-code $?
+  ./cc-test-reporter after-build -t coverage.py --exit-code $?
 fi
